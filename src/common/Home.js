62bb4557568e6d39e996d15433c5c3261484699c
zeqiang.chen <1209120868@qq.com>
2019-08-06 17:30:32 +0800

    [zeqiang] fix: change the style of addProduct page

--- src/common/Home.js
+++ src/common/Home.js
@@ -3,15 +3,16 @@ import Header from "./Header";
 import './home.less'
 import {Route, Switch} from "react-router-dom";
 import AddProduct from "../product/AddProduct";
+import Products from "../product/Products";
 
 class Home extends Component {
   render() {
     return (
       <div id={'container'}>
         <Header/>
-
         <Switch>
           <Route exact path={'/product/add'} component={AddProduct} />
+          <Route exact path={'/products'} component={Products} />
         </Switch>
       </div>
     );
---
+++ src/product/Products.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './products.less';
+
+class Products extends Component {
+  render() {
+    return (
+      <main id='productsBox'>
+        Products
+
+      </main>
+    );
+  }
+}
+
+export default Products;
