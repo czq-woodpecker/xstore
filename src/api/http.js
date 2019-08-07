b3cfe214f2d89d12be1eab24b744b0927aec3594
zeqiang.chen <1209120868@qq.com>
2019-08-07 22:35:21 +0800

    [zeqiang] refactor: 重构http.js

--- src/api/http.js
+++ src/api/http.js
@@ -1,29 +1,17 @@
 export function getData(url) {
-  return new Promise((resolve, reject) => {
-    fetch(url)
-      .then(response => resolve(response))
-      .catch(error => reject(error));
-  })
+  return fetch(url);
 }
 
 export function deleteData(url) {
-  return new Promise((resolve, reject) => {
-    fetch(url, {method: 'DELETE'})
-      .then(response => resolve(response))
-      .catch(error => reject(error))
-  })
+  return fetch(url, {method: 'DELETE'})
 }
 
 export function postData(url, data) {
-  return new Promise((resolve, reject) => {
-    fetch(url, {
-      body: JSON.stringify(data),
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json;charset=UTF-8'
-      },
-    })
-      .then(response => resolve(response))
+  return fetch(url, {
+    body: JSON.stringify(data),
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=UTF-8'
+    }
   })
 }
-
