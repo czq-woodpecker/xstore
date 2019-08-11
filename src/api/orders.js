dce84970059075c1a1ce3f579e59f1f26559ad84
zeqiang.chen <1209120868@qq.com>
2019-08-12 00:18:11 +0800

    [zeqiang] feat: 完成删除订单功能

--- src/api/orders.js
+++ src/api/orders.js
@@ -1,5 +1,7 @@
-import {BASE_URL, getData, postData} from "./http";
+import {BASE_URL, deleteData, getData, postData} from "./http";
 
 export const addOrder = productId => postData(BASE_URL + '/orders', productId);
 
 export const getOrders = () => getData(BASE_URL + '/orders');
+
+export const deleteOrder = orderId => deleteData(BASE_URL + '/orders/' + orderId);
--- src/order/Orders.js
+++ src/order/Orders.js
@@ -2,8 +2,14 @@ import React, {Component} from 'react';
 import './orders.less';
 import {connect} from "react-redux";
 import {getOrdersAction} from "../actions/orderAction";
+import {deleteOrder} from "../api/orders";
 
 class Orders extends Component {
+
+
+  constructor(props, context) {
+    super(props, context);
+  }
 
   componentDidMount() {
     this.props.getOrders();
@@ -14,30 +20,61 @@ class Orders extends Component {
       <main id={'ordersBox'}>
         <table>
           <thead>
-            <tr>
-              <td>名字</td>
-              <td>单价</td>
-              <td>数量</td>
-              <td>单位</td>
-              <td>操作</td>
-            </tr>
+          <tr>
+            <td>名字</td>
+            <td>单价</td>
+            <td>数量</td>
+            <td>单位</td>
+            <td>操作</td>
+          </tr>
           </thead>
           <tbody>
-          {this.props.orders.map((order, index) => (
-            <tr key={index}>
-              <td>{order.productName}</td>
-              <td>{order.productPrice}</td>
-              <td>{order.amount}</td>
-              <td>{order.productUnit}</td>
-              <td>
-                <button>删除</button>
-              </td>
-            </tr>
-          ))}
+          {this.getOrderItems(this.props.orders)}
           </tbody>
         </table>
       </main>
     );
+  }
+
+  getOrderItems(orders) {
+    if (orders == null || orders.length == 0) {
+      return (
+        <tr>
+          <td>
+            暂无订单，返回商城继续购买
+          </td>
+        </tr>
+      )
+    } else {
+      return orders.map((order, index) => (
+        <tr key={index}>
+          <td>{order.productName}</td>
+          <td>{order.productPrice}</td>
+          <td>{order.amount}</td>
+          <td>{order.productUnit}</td>
+          <td>
+            <button onClick={() => this.deleteOrder(order.orderId)}>删除</button>
+          </td>
+        </tr>
+      ))
+    }
+  }
+
+  deleteOrder(orderId) {
+    deleteOrder(orderId)
+      .then(response => {
+        if (response.ok) {
+          alert('删除成功');
+          this.props.getOrders();
+        } else {
+          response.text().then(errorMessage => {
+            alert(errorMessage);
+          })
+        }
+      })
+      .catch(() => {
+        alert('连接失败');
+      })
   }
 }
 
